6b1b4e8bc1a912eb7c9d90fc971338b63b257e2f
Thomas Holmes <thomasholmes@outlook.com>
2020-11-24 20:52:07 +0000

    Updates

---
+++ precache-manifest.7e69499fdfbe11964cbfbce19890ad27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0996cdfa0ff62557a27f2cbbd678a4",
+    "url": "/duck-game/index.html"
+  },
+  {
+    "revision": "d18d3da4d2a2e0d65b12",
+    "url": "/duck-game/static/css/main.26e39934.chunk.css"
+  },
+  {
+    "revision": "9dfed0233b75bd16088f",
+    "url": "/duck-game/static/js/2.6d613665.chunk.js"
+  },
+  {
+    "revision": "d18d3da4d2a2e0d65b12",
+    "url": "/duck-game/static/js/main.c4000c04.chunk.js"
+  },
+  {
+    "revision": "9d0f0ce4c18413338061",
+    "url": "/duck-game/static/js/runtime-main.8a583ca5.js"
+  }
+]);
